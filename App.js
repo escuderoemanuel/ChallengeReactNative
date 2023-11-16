00d149775099286be7559bf461bbe9f199b9da90
Emanuel Escudero <escudero.mdz@gmail.com>
2023-11-16 00:33:35 -0300

    Primer Desafío Entregable

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native</Text>
+      <Text>Hola, Coder!</Text>
+      <Text>Primer Desafío Entregable: 'Creando App Expo'</Text>
       <StatusBar style="auto" />
     </View>
   );
